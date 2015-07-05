ad6c427a4542478798c3e1eb81a9c90dbb863b7c
mattmcd <matt@matt-mcdonnell.com>
2015-07-05 10:29:30 +0100

    Fix ANTLR python modeldef program.

--- modeldef/program.py
+++ modeldef/program.py
@@ -1,23 +1,33 @@
 from antlr4 import *
-from ExprLexer import ExprLexer
-from ExprParser import ExprParser
-from EvalListener import EvalListener
+from mdef.ModelDefLexer import ModelDefLexer
+from mdef.ModelDefParser import ModelDefParser
+#from EvalListener import EvalListener
 
-def main(argv):
+def main(input_str):
 
-    input = "a=1\na\nclear\na\na=3\na\nb=2+a\nb+a\n"
+    # input = "a=1\na\nclear\na\na=3\na\nb=2+a\nb+a\n"
 
-    chars = InputStream.InputStream(input);
-    lexer = ExprLexer(chars);
+    print input_str
+    chars = InputStream.InputStream(input_str);
+    lexer = ModelDefLexer(chars);
     tokens = CommonTokenStream(lexer);
-    parser = ExprParser(tokens);
-    tree = parser.prog();
+    parser = ModelDefParser(tokens);
+    tree = parser.modeldef();
 
+    print tree
     # Actually do the walking
-    evaluator = EvalListener();
-    walker = ParseTreeWalker();
-    walker.walk(evaluator, tree);
+    #evaluator = EvalListener();
+    #walker = ParseTreeWalker();
+    #walker.walk(evaluator, tree);
 
 if __name__ == '__main__':
     import sys
-    main(sys.argv)
+    if len(sys.argv) > 1:
+        infile = sys.argv[1]
+    else:
+        infile = 'test.txt'
+    f = open(infile, 'r')
+    input_str = [x for x in f.readlines()]
+    f.close()
+
+    map(main, input_str)
