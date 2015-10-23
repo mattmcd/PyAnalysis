dab8aca146fc4c6ee2c66ae60ab473190b338d85
mattmcd <matt@matt-mcdonnell.com>
2015-10-23 15:14:01 +0100

    Read prices from Google Finance

--- mda/finance.py
+++ mda/finance.py
@@ -20,4 +20,29 @@ class LseReader:
         txt = urllib2.urlopen(self.prefixURL +
                               'q={Ticker}&x=LON&i={Interval}&p={Period}d&f=d,o,h,l,c,v'.format(
             Ticker=ticker, Interval=interval, Period=period)).read()
-        return txt
+        lines = txt.split('\n')
+        cols = lines[4].lower().split('=')[1].split(',')
+        recs  = [dict(zip(cols, line.split(','))) for line in lines[7:-1]]
+        df = pd.DataFrame(recs, columns=cols)
+        df[cols[1:]] = df[cols[1:]].convert_objects(convert_numeric=True)
+        dates = np.zeros(df.date.shape, dtype=pd.Timestamp)
+        last_date = pd.Timestamp(0)
+        for i, date_str in enumerate(df.date):
+            if not date_str:
+                continue
+            if date_str[0].lower() == 'a':
+                last_date = pd.to_datetime(int(date_str[1:]), unit='s')
+                dates[i] = last_date
+            else:
+                # Offset
+                this_date = last_date + pd.to_timedelta(int(date_str)*interval, unit='s')
+                dates[i] = this_date
+        df['date'] = dates
+        df.index = df.date
+
+        return df
+
+
+reader = LseReader()
+tickers = reader.ftse100.Ticker
+df = reader.read_history(tickers[0])
