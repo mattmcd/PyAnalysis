0bb89f373b1b67e23b3fb7065598463257a1deff
mattmcd <matt@matt-mcdonnell.com>
2017-04-17 10:47:15 +0100

    Fix parsing for multiple timezone offsets (BST transition)

--- mda/io/google_finance.py
+++ mda/io/google_finance.py
@@ -45,18 +45,16 @@ class LseReader:
         :return: df : downloaded price data as pandas DataFrame
         """
         txt, interval = self.read_url(ticker, interval, period)
-        df = parse_text(txt, interval)
+        df = parse_text(txt, ticker=ticker)
         return df
 
 
-def parse_text(txt, interval, ticker=None):
-    lines = txt.split('\n')
-    cols = lines[4].lower().split('=')[1].split(',')
-    recs = [dict(zip(cols, line.split(','))) for line in lines[7:-1]]
+def parse_ohlc(cols, interval, recs, ticker):
     df = pd.DataFrame(recs, columns=cols)
     for col in cols[1:]:
         df[col] = pd.to_numeric(df[col])
     dates = np.zeros(df.date.shape, dtype=pd.Timestamp)
+    period = np.zeros(df.date.shape, dtype=int)
     last_date = pd.Timestamp(0)
     for i, date_str in enumerate(df.date):
         if not date_str:
@@ -64,30 +62,63 @@ def parse_text(txt, interval, ticker=None):
         if date_str[0].lower() == 'a':
             last_date = pd.to_datetime(int(date_str[1:]), unit='s')
             dates[i] = last_date
+            period[i] = 0
         else:
             # Offset
             try:
                 this_date = last_date + pd.to_timedelta(int(date_str) * interval, unit='s')
                 dates[i] = this_date
+                period[i] = int(date_str)
             except ValueError as ex:
                 print(ticker + ': ' + ex.message)
     df['date'] = dates
+    df['period'] = period
     if ticker:
         df['ticker'] = ticker
-
     return df
 
 
-def read_file(ticker_file, datestr, interval=60):
+def parse_text(txt, ticker=None):
+    lines = txt.split('\n')
+    # Find line where data starts
+    data_ind = next(i for i, el in enumerate(lines) if 'DATA=' in el)
+    # Find lines where the timezone offset is specified.  If there has been no
+    # change in timezone there will only be one, immediately after the
+    # DATA= line, however when the clocks change (BST) there will be two
+    tz_lines = [i for i, el in enumerate(lines) if 'TIMEZONE_OFFSET=' in el]
+    if len(tz_lines) == 0:
+        return None
+    tz_lines.append(-1)
+    # Extract the header key value pairs.  We ignore the first line of the file
+    # which indicates the exchange.
+    header = {k.lower(): v.lower() for (k, v) in
+              [tuple(line.split('=')) for line in lines[1:data_ind]]}
+    header['columns'] = header['columns'].split(',')
+    for field in ['interval', 'market_close_minute', 'market_open_minute']:
+        header[field] = int(header[field])
+    # Extract the price data, appending column for the timezone offset
+    res = []
+    for i in range(0, len(tz_lines)-1):
+        tz_offset = int(lines[tz_lines[i]].split('=')[1])
+        recs = [dict(zip(header['columns'], line.split(',')))
+                for line in lines[tz_lines[i]+1:tz_lines[i+1]]]
+        df = parse_ohlc(header['columns'], header['interval'], recs, ticker=ticker)
+        df['tz_offset'] = tz_offset
+        res.append(df)
+    df = pd.concat(res).reset_index(drop=True)
+    return df
+
+
+def read_file(ticker_file, datestr):
     with open(mda.data_dir('FTSE100', datestr, ticker_file)) as f:
-        return parse_text(f.read(), interval, ticker=ticker_file.split('.')[0])
+        return parse_text(f.read(), ticker=ticker_file.split('.')[0])
 
 
-def read_dir(datestr, interval=60, print_date=False):
+def read_dir(datestr, print_date=False):
     if print_date:
         print(datestr)
     return pd.concat(
-        [read_file(f, datestr, interval)
+        [read_file(f, datestr)
          for f in os.listdir(mda.data_dir('FTSE100', datestr))])
 
 
